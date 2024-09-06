3c3aa17c7880075ad5fadefa593c0a85dc0d03c9
Albani Rajata Malik <albeebro002@gmail.com>
2024-09-06 08:59:25 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760080 / Albani Rajata Malik';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'albani '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan id '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'albani '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
